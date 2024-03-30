45721e71d6ed1108c6027dd2226ef9fba07c813d
Grant Davis <grantdavis303@gmail.com>
2024-03-29 23:57:06 -0600

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -42,4 +42,6 @@ Rails.application.routes.draw do
   get "/applications/:id", to: "applications#show"
   post "/applications", to: "applications#create"
 
+  get "/admin/shelters", to: "admin#index"
+
 end
