2a537c6735fbc6a96b1cecea4a0aa1fb10af2e28
Steddman Bell <steddfast@gmail.com>
2024-03-29 19:56:45 -0700

    Passing User story 1

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,12 @@
 class Application < ApplicationRecord
-    
   has_many :application_pets
   has_many :pets, through: :application_pets
   
+  def self.pets_applying_for
+    Pet.joins(application_pets: {application: :shelter}).distinct
+  end
+
+  def self.unique_list_pets(id)
+    Pet.joins(:applications).distinct.where("application_id = ?", id)
+  end
 end
---
+++ db/migrate/20240330015008_add_column_to_application.rb
@@ -0,0 +1,5 @@
+class AddColumnToApplication < ActiveRecord::Migration[7.1]
+  def change
+    add_column :applications, :status, :string
+  end
+end
