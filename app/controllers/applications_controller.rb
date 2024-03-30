87c034b35153a14f63154d10fc976bc372a0356e
Grant Davis <grantdavis303@gmail.com>
2024-03-29 22:00:00 -0600

    Make New and Create Methods w/ Strong Params

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationsController < ApplicationController
+  def new
+  end
+
   def show
     @application = Application.find(params[:id])
     @app = Application.unique_list_pets(@application.id)
+  end
+
+  def create
+    Application.create!(applications_params)
+
+    redirect_to "/application/:id"
+  end
+
+  private
+
+  def applications_params
+    params_permit(:name, :street_address, :city, :state, :zip_code, :description, :status)
   end
 end
