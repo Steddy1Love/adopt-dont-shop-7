c451a841c261937210281dc4f6be4b49c7dcd1cb
Grant Davis <grantdavis303@gmail.com>
2024-03-30 22:38:18 -0600

    Remove variable methods

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,7 @@
 class AdminController < ApplicationController
     def index
-        @admin_shelters = Shelter.find_by_sql("SELECT name FROM shelters ORDER BY shelters.name DESC;")
-        @pending_app_shelters = Shelter.joins(:applications).where("applications.status = 'Pending'").distinct;
+        #@admin_shelters = Shelter.find_by_sql("SELECT name FROM shelters ORDER BY shelters.name DESC;")
+        #@pending_app_shelters = Shelter.with_pending_apps
+        #@pending_app_shelters = Shelter.joins(:applications).where("applications.status = 'Pending'").distinct;
     end
 end
