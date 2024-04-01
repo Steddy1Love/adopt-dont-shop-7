985b9debb30de26326e07183495eb19319501493
Grant Davis <grantdavis303@gmail.com>
2024-04-01 14:41:15 -0600

    Update Models

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,7 @@
 class Application < ApplicationRecord
+
+  validates :name, :street_address, :city, :state, :zip_code, :description, presence: true
+
   has_many :application_pets
   has_many :pets, through: :application_pets
   
