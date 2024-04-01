e32fc66edc1b0b6db8a5efa46fbfaee2d4d27b7c
Mel Langhoff <mlanghoff@uwalumni.com>
2024-04-01 17:27:11 -0600

    search functionality works

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-  
-  def self.search(search_params)
-    where("name ILIKE ?", "%#{search_params}%")
+      
+  def self.search(query_params)
+    where("name ILIKE ?", "%#{query_params}%")
   end
 end
