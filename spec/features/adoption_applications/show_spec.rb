fe820fe9209c20e9226b9fceffeeb1239a6a264e
Mel Langhoff <mlanghoff@uwalumni.com>
2024-04-01 14:46:57 -0600

    start spec file

---
+++ spec/features/adoption_applications/show_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Application Show Page' do
+  before :each do
+    
+  end
+
+  describe 'search for pets for application' do
+    it 'has a search for pets by name' do
+
+    end
+
+    it 'can search a pet by name' do
+
+    end
+
+    it 'can search case insensitive' do
+
+    end
+
+    it 'can search with a partial query' do
+
+    end
+  end
+
+  describe 'user story 5' do
+    it 'has a button to add a pet to an application after searching by name' do
+
+    end
+
+    it 'adds a pet to an application and redirects to application show page with new pet added' do
+
+    end
+  end
+end
