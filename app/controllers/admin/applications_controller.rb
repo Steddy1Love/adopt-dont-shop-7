1a17f823caada92cdaa7c8c40fa0c5d26f33e5ef
Steddman Bell <steddfast@gmail.com>
2024-04-03 09:18:32 -0700

    US 12 & 13 completed

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -4,26 +4,25 @@ class Admin::ApplicationsController < ApplicationController
   end
 
   def show
-    @application_pets = []
     @application = Application.find(params[:id])
     @pets = @application.pets
-    @application_pets = ApplicationPet.find(@application.id, @pet_id)
+    @application_pets = ApplicationPet.where(application_id: @application.id)
   end
 
   def update
     application = Application.find(params[:id])
 
     if params.include? :approved_pet
-        pet_id = params[:approved_pet]
-        application_pet = ApplicationPet.where({pet_id: pet_id, application_id: application.id}).first
-        application_pet.update(status: "Approved")
+      pet_id = params[:approved_pet]
+      application_pet = ApplicationPet.where({pet_id: pet_id, application_id: application.id}).first
+      application_pet.update(status: "Approved")
+      redirect_to "/admin/applications/#{application.id}"
 
     elsif params.include? :rejected_pet
-        pet_id = params[:rejected_pet]
-        application_pet = ApplicationPet.where({pet_id: pet_id, application_id: application.id}).first
-        application_pet.update(status: "Rejected")
-
+      pet_id = params[:rejected_pet]
+      application_pet = ApplicationPet.where({pet_id: pet_id, application_id: application.id}).first
+      application_pet.update(status: "Rejected")
+      redirect_to "/admin/applications/#{application.id}"
     end
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_185718) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_153618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_185718) do
     t.bigint "pet_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "In Progress"
     t.index ["application_id"], name: "index_application_pets_on_application_id"
     t.index ["pet_id"], name: "index_application_pets_on_pet_id"
   end
