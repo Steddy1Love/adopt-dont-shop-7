f05285d6c90572a606e765db0dde36d800181bf6
Grant Davis <grantdavis303@gmail.com>
2024-04-03 09:23:04 -0600

    Update

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -10,9 +10,8 @@ class Admin::ApplicationsController < ApplicationController
   end
 
   def update
-
     application = Application.find(params[:id])
-    #binding.pry
+
     if params.include? :approved_pet
         pet_id = params[:approved_pet]
         application_pet = ApplicationPet.where({pet_id: pet_id, application_id: application.id}).first
@@ -24,13 +23,6 @@ class Admin::ApplicationsController < ApplicationController
         application_pet.update(status: "Rejected")
 
     end
-    #binding.pry
-    # if params[:approved_pet].present?
-    #     pet_id = params[:approved_pet]
-    #     ApplicationPet.change_status_to_approved(@application.id, pet_id)
-    # elsif params[:rejected_pet].present?
-    #     pet_id = params[:rejected_pet]
-    #     ApplicationPet.change_status_to_rejected(@application.id, pet_id)
   end
 
 end
