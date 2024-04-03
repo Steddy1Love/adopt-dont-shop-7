36de75119f70771236b670d57e5689d494b7d9f3
Steddman Bell <steddfast@gmail.com>
2024-04-02 21:16:44 -0700

    Saving and pushing work to collaborate tomorrow

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -4,19 +4,21 @@ class Admin::ApplicationsController < ApplicationController
   end
 
   def show
+    @application_pets = []
     @application = Application.find(params[:id])
     @pets = @application.pets
-    @application_pets = ApplicationPet.find(@application.id, pet_id)
+    @pets.each{|pet|
+     binding.pry @application_pets << ApplicationPet.find_by(pet_id: pet.id, application_id: @application.id)}
   end
 
   def update
      binding.pry
      if params[:approved_pet].present?
       pet_id = params[:approved_pet]
-      
+      @application_pets = ApplicationPet.find(@application.id, pet_id)
       ApplicationPet.change_status_to_approved(@application.id, pet_id)
     elsif params[:rejected_pet].present?
-
+      @application_pets = ApplicationPet.find(@application.id, pet_id)
       pet_id = params[:rejected_pet]
       ApplicationPet.change_status_to_rejected(@application.id, pet_id)
     end
