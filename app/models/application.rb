70a275ec17841759e2520c37ad8cda840c82059f
Grant Davis <grantdavis303@gmail.com>
2024-04-03 22:16:18 -0600

    Update Formatting

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,4 @@
 class Application < ApplicationRecord
-
   validates :name, :street_address, :city, :state, :zip_code, :description, presence: true
 
   has_many :application_pets
@@ -8,10 +7,6 @@ class Application < ApplicationRecord
   def self.pets_applying_for
     Pet.joins(application_pets: {application: :shelter}).distinct
   end
-
-#   def change_status(new_status)
-#     self.update(new_status)
-#   end
 
   def add_pet(pet_id)
     pet = Pet.find(pet_id)
